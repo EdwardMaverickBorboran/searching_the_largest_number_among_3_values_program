b4116955860e67d35bbbd90322b8041aaabf6400
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-07 01:48:31 +0800

    Creating the frame for main tab

--- finding_the_biggest_number.py
+++ finding_the_biggest_number.py
@@ -8,12 +8,18 @@
 import tkinter
 
 
-#Creating main window/tab
+#Creating the main tab
 Main_Tab = tkinter.Tk()
 Main_Tab.title ("Biggest Number?")
-Main_Tab.geometry ("350x500")
+Main_Tab.geometry ("350x400")
 
-#Can't resize the main window/tab
+#Creating the frame for main tab
+Main_Frame = tkinter.Frame(Main_Tab)
+Main_Frame.pack(expand=True, fill="both", padx=10, pady=10)
+Main_Frame_Label = tkinter.Label(Main_Tab, text="Which is the biggest number?", font=('Figtree', 15, 'bold'))
+Main_Frame_Label.place(relx=0.5, rely=0.09, anchor= "center")
+
+#Can't resize the main tab
 Main_Tab.resizable (False, False)
 
 #Looping
