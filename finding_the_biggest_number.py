0a4b76972fd890e754bada1dcc2518c2233203bd
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-07 02:13:07 +0800

    Adding button to proceed

--- finding_the_biggest_number.py
+++ finding_the_biggest_number.py
@@ -4,8 +4,12 @@
 
 #Need to ask user to input 3 numbers.
 #Find and print the biggest number using if-else statement.
+#Record the demo presenting the programs (less than 2 mins only)
+#Submit the demo directly to Sir Danilo's messenger
+#Deadline: BEFORE JAN 13, 2024!!!
 
 import tkinter
+from tkinter import Button
 
 #Pseudo Code
 
@@ -29,6 +33,10 @@ Second_Num_Entry.place(relx=0.5, rely=0.3, anchor="center")
 
 Third_Number_Entry = tkinter.Entry(Main_Tab)
 Third_Number_Entry.place(relx=0.5, rely=0.4, anchor="center")
+
+#Adding button to proceed
+Proceed_Button= Button(Main_Tab, text="Proceed", command=open,font=('Figtree', 12, 'bold'))
+Proceed_Button.place(relx=0.5, rely=0.52, anchor="center")
 
 #Can't resize the main tab
 Main_Tab.resizable (False, False)
