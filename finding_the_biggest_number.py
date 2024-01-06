2651b5c8bcc4c823388279af95a624e2ceb847eb
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-07 00:34:42 +0800

    creating the main window/tab

---
+++ finding_the_biggest_number.py
@@ -0,0 +1,20 @@
+#Homework 4
+
+#Searching for the largest number among 3 values program
+
+#Need to ask user to input 3 numbers.
+#Find and print the biggest number using if-else statement.
+
+import tkinter
+
+
+#Creating main window/tab
+Main_Tab = tkinter.Tk()
+Main_Tab.title ("Biggest Number?")
+Main_Tab.geometry ("350x500")
+
+#Can't resize the main window/tab
+Main_Tab.resizable (False, False)
+
+#Looping
+Main_Tab.mainloop()
