8fdebc62cb3db3dcf5b66f972e97728b20fa5c13
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-07 13:35:29 +0800

    Fixed the layout issue and then added a frame for the entry widgets and results part

--- finding_the_biggest_number.py
+++ finding_the_biggest_number.py
@@ -14,6 +14,36 @@ from tkinter import Button
 # Pseudo Code
 
 # Proceeds into searching for the biggest number among the 3 given values by the user
+def open():
+    # Destroy Main Tab
+    Main_Tab.destroy()
+
+    # Creating 2nd Tab
+    Next_Tab = tkinter.Tk()
+    Next_Tab.title ("Input the Numbers")
+    Next_Tab.geometry ("350x450")
+
+    Input_Num_Frame = tkinter.Frame(Next_Tab)
+    Input_Num_Frame.pack(expand=True, fill="both", padx=10, pady=10)
+    Input_Num_Frame_Label = tkinter.Label(Next_Tab, text="Please enter 3 Numbers", font=('Figtree', 16, 'bold'))
+    Input_Num_Frame_Label.place(relx=0.5, rely=0.1, anchor= "center")
+
+
+    # Adding entry widgets
+    First_Number_Entry = tkinter.Entry(Next_Tab)
+    First_Number_Entry.place(relx=0.5, rely=0.2, anchor="center")
+
+    Second_Number_Entry = tkinter.Entry(Next_Tab)
+    Second_Number_Entry.place(relx=0.5, rely=0.3, anchor="center")
+
+    Third_Number_Entry = tkinter.Entry(Next_Tab)
+    Third_Number_Entry.place(relx=0.5, rely=0.4, anchor="center")
+
+    Results_Frame = tkinter.Frame(Next_Tab)
+    Results_Frame.pack(expand=True, fill="both", padx=10, pady=10)
+    Results_Frame_Label = tkinter.Label(Next_Tab, text="Results:", font=('Figtree', 16, 'bold'))
+    Results_Frame_Label.place(relx=0.5, rely=0.58, anchor= "center")
+
 
 # Creating the main tab
 Main_Tab = tkinter.Tk()
@@ -32,20 +62,6 @@ Proceed_Button.place(relx=0.35, rely=0.6, anchor="center")
 
 Cancel_Button = Button(Main_Tab, text="Cancel", command=exit, font=('Figtree', 12, 'bold'))
 Cancel_Button.place(relx=0.65, rely=0.6, anchor="center")
-
-# Adding entry widgets
-#First_Number_Entry = tkinter.Entry(Main_Tab)
-#First_Number_Entry.place(relx=0.5, rely=0.2, anchor="center")
-
-#Second_Number_Entry = tkinter.Entry(Main_Tab)
-#Second_Number_Entry.place(relx=0.5, rely=0.3, anchor="center")
-
-#Third_Number_Entry = tkinter.Entry(Main_Tab)
-#Third_Number_Entry.place(relx=0.5, rely=0.4, anchor="center")
-
-# Adding button to proceed
-#Proceed_Button= Button(Main_Tab, text="Proceed", font=('Figtree', 12, 'bold'))
-#Proceed_Button.place(relx=0.5, rely=0.52, anchor="center")
 
 # Can't resize the main tab
 Main_Tab.resizable (False, False)
