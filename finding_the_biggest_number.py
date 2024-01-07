f362d933fe079f89900fd9b3d3fdf9c5751be391
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-08 01:29:29 +0800

    Adjusted some parts

--- finding_the_biggest_number.py
+++ finding_the_biggest_number.py
@@ -18,14 +18,10 @@ def open():
     if any (blank == "" for blank in (first_digit_entry.get(), second_digit_entry.get(), third_digit_entry.get())):
         messagebox.showerror(title="Error", message="Please fill out the blanks.")
         return
-    
-    first_digit = first_digit_entry.get()
-    second_digit = second_digit_entry.get()
-    third_digit = third_digit_entry.get()
-    
-    if (first_digit_entry.get() and second_digit_entry.get() and third_digit_entry.get()).isalpha():
-        messagebox.showwarning(title= "Invalid", message="Please enter using NUMBERS ONLY.")
-        return
+
+    first_digit = float(first_digit_entry.get())
+    second_digit = float(second_digit_entry.get())
+    third_digit = float(third_digit_entry.get())
 
     # Destroy Main Tab
     main_tab.destroy()
@@ -39,29 +35,29 @@ def open():
     results_frame = tkinter.Frame(next_tab)
     results_frame.pack(expand=True, fill="both", padx=10, pady=10)
     results_frame_label = tkinter.Label(next_tab, text="Results:", font=('Figtree', 30, 'bold'))
-    results_frame_label.place(relx=0.5, rely=0.12, anchor= "center")
+    results_frame_label.grid(padx=0.5, pady=0.12, anchor= "center")
 
     # Title text for the biggest number
     biggest_num_text = tkinter.Label(results_frame, text="The biggest number is:", font=('Figtree', 18, 'bold'))
-    biggest_num_text.place(relx=0.5, rely=0.36, anchor= "center")
+    biggest_num_text.grid(padx=0.5, pady=0.36, anchor= "center")
 
     # Adding an exit button
     exit_button = Button(next_tab, text="Done", command=exit, font=('Figtree', 15, 'bold'))
-    exit_button.place(relx=0.5, rely=0.86, anchor="center")
+    exit_button.grid(padx=0.5, pady=0.86, anchor="center")
 
     # Getting the numbers
     def all(first_digit, second_digit, third_digit):
         if (first_digit >= second_digit) and (first_digit >= third_digit):
-            biggest_num = first_digit
+            return first_digit
         elif (second_digit >= first_digit) and (second_digit >= third_digit):
-            biggest_num = second_digit
+            return second_digit
         else:
-            biggest_num = third_digit
+            return third_digit
         
     # Finding out what is the biggest number among the 3
     biggest_num = all(first_digit, second_digit, third_digit)
     biggest_number_results = tkinter.Label(next_tab, text= biggest_num, font=('Figtree', 30, 'bold'))
-    biggest_number_results.place(relx=0.5, rely=0.575, anchor= "center")
+    biggest_number_results.grid(padx=0.5, pady=0.575, anchor= "center")
 
 
 # Creating the main tab
@@ -73,24 +69,24 @@ main_tab.geometry ("350x300")
 main_frame = tkinter.Frame(main_tab)
 main_frame.pack(expand=True, fill="both", padx=10, pady=10)
 main_frame_label = tkinter.Label(main_tab, text="Finding the Biggest Number", font=('Figtree', 16, 'bold'))
-main_frame_label.place(relx=0.5, rely=0.15, anchor= "center")
+main_frame_label.grid(padx=0.5, pady=0.15, anchor= "center")
 
 # Adding entry widgets
 first_digit_entry = tkinter.Entry(main_frame)
-first_digit_entry.place(relx=0.5, rely=0.33, anchor="center")
+first_digit_entry.grid(padx=0.5, pady=0.33, anchor="center")
 
 second_digit_entry = tkinter.Entry(main_frame)
-second_digit_entry.place(relx=0.5, rely=0.48, anchor="center")
+second_digit_entry.grid(padx=0.5, pady=0.48, anchor="center")
 
 third_digit_entry = tkinter.Entry(main_frame)
-third_digit_entry.place(relx=0.5, rely=0.63, anchor="center")
+third_digit_entry.grid(padx=0.5, pady=0.63, anchor="center")
 
 # Adding buttons to proceed and cancel
 proceed_button = Button(main_tab, text="Proceed", command=open, font=('Figtree', 12, 'bold'))
-proceed_button.place(relx=0.35, rely=0.83, anchor="center")
+proceed_button.grid(padx=0.35, pady=0.83, anchor="center")
 
 cancel_button = Button(main_tab, text="Cancel", command=exit, font=('Figtree', 12, 'bold'))
-cancel_button.place(relx=0.65, rely=0.83, anchor="center")
+cancel_button.grid(padx=0.65, pady=0.83, anchor="center")
 
 # Can't resize the main tab
 main_tab.resizable (False, False)
