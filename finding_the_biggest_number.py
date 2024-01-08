1def3c4c912adc8951e3c015677e6572ca72e1c7
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-08 16:50:22 +0800

    Finished designing the program

--- finding_the_biggest_number.py
+++ finding_the_biggest_number.py
@@ -33,16 +33,16 @@ def open():
             # Adding a results frame
             results_frame = customtkinter.CTkFrame(main_tab)
             results_frame.pack(expand=True, fill="both", padx=10, pady=10)
-            results_frame_label = customtkinter.CTkLabel(results_frame, text="Results:", font=('Figtree', 30, 'bold'))
-            results_frame_label.place(relx=0.5, rely=0.15, anchor= "center")
+            results_frame_label = customtkinter.CTkLabel(results_frame, text="Outcome", font=('Consolas', 50, 'bold'))
+            results_frame_label.place(relx=0.5, rely=0.19, anchor= "center")
 
             # Title text for the biggest number
-            biggest_num_text = customtkinter.CTkLabel(results_frame, text="The biggest number is:", font=('Figtree', 18, 'bold'))
-            biggest_num_text.place(relx=0.5, rely=0.36, anchor= "center")
+            biggest_num_text = customtkinter.CTkLabel(results_frame, text="The biggest number is:", font=('Consolas', 25, 'bold'))
+            biggest_num_text.place(relx=0.5, rely=0.43, anchor= "center")
 
             # Adding an exit button
-            exit_button = customtkinter.CTkButton(main_tab, text="Done", command=exit, font=('Figtree', 15, 'bold'))
-            exit_button.place(relx=0.5, rely=0.86, anchor="center")
+            exit_button = customtkinter.CTkButton(main_tab, text="Done", command=exit, font=('Consolas', 15, 'bold'), fg_color="red3", bg_color='gainsboro')
+            exit_button.place(relx=0.5, rely=0.88, anchor="center")
 
             # Main funtion in finding the biggest number
             if (first_digit >= second_digit) and (first_digit >= third_digit):
@@ -53,8 +53,8 @@ def open():
                 biggest = third_digit
 
             # Finding out what is the biggest number among the 3
-            biggest_number_results = customtkinter.CTkLabel(results_frame, text= biggest, font=('Figtree', 30, 'bold'))
-            biggest_number_results.place(relx=0.5, rely=0.575, anchor= "center")
+            biggest_number_results = customtkinter.CTkLabel(results_frame, text= biggest, font=('Consolas', 55, 'bold'), text_color=('midnight blue'))
+            biggest_number_results.place(relx=0.5, rely=0.58, anchor= "center")
 
         else:
             messagebox.showerror(title="Invalid", message="Please input numbers only.")  
@@ -66,31 +66,31 @@ def open():
 customtkinter.set_appearance_mode("light")
 
 # Creating the main tab
-main_tab = customtkinter.CTk()
+main_tab = customtkinter.CTk(fg_color='white smoke')
 main_tab.title ("Biggest Number?") 
 main_tab.geometry ("350x380")
 
 # Creating the main frame for main tab
-main_frame = customtkinter.CTkFrame(main_tab)
+main_frame = customtkinter.CTkFrame(main_tab, fg_color='gainsboro')
 main_frame.pack(expand=True, fill="both", padx=10, pady=10)
-main_frame_label = customtkinter.CTkLabel(main_frame, text="Finding the Biggest Number", font=('Figtree', 21, 'bold'))
+main_frame_label = customtkinter.CTkLabel(main_frame, text="Let's find the biggest number!", font=('Calibri', 23, 'bold'))
 main_frame_label.place(relx=0.5, rely=0.135, anchor= "center")
 
 # Adding entry widgets and its placements
-input_first_digit = customtkinter.CTkEntry(main_frame, placeholder_text="First Number",placeholder_text_color="black", width=225, corner_radius=8)
+input_first_digit = customtkinter.CTkEntry(main_frame, placeholder_text="Input the 1st number here..",placeholder_text_color="black", width=225, corner_radius=8, font=('Consolas', 12.5, 'bold'))
 input_first_digit.place(relx=0.5, rely=0.31, anchor="center")
 
-input_second_digit = customtkinter.CTkEntry(main_frame, placeholder_text="Second Number",placeholder_text_color="black", width=225, corner_radius=8)
+input_second_digit = customtkinter.CTkEntry(main_frame, placeholder_text="Input the 2nd number here..",placeholder_text_color="black", width=225, corner_radius=8, font=('Consolas', 12.5, 'bold'))
 input_second_digit.place(relx=0.5, rely=0.45, anchor="center")
 
-input_third_digit = customtkinter.CTkEntry(main_frame, placeholder_text="Third Number",placeholder_text_color="black", width=225, corner_radius=8)
+input_third_digit = customtkinter.CTkEntry(main_frame, placeholder_text="Input the 3rd number here..",placeholder_text_color="black", width=225, corner_radius=8, font=('Consolas', 12.5, 'bold'))
 input_third_digit.place(relx=0.5, rely=0.59, anchor="center")
 
 # Adding buttons to proceed and cancel
-proceed_button = customtkinter.CTkButton(main_tab, text="Proceed", command=open, font=('Figtree', 13, 'bold'), fg_color="green")
+proceed_button = customtkinter.CTkButton(main_tab, text="Proceed", command=open, font=('Calibri', 15, 'bold'), fg_color="forest green", bg_color='gainsboro')
 proceed_button.place(relx=0.5, rely=0.75, anchor="center")
 
-cancel_button = customtkinter.CTkButton(main_tab, text="Cancel", command=exit, font=('Figtree', 13, 'bold'), fg_color="red")
+cancel_button = customtkinter.CTkButton(main_tab, text="Cancel", command=exit, font=('Calibri', 15, 'bold'), fg_color="red3", bg_color='gainsboro')
 cancel_button.place(relx=0.5, rely=0.85, anchor="center")
 
 # Can't resize the main tab
